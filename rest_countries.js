8f64aec913b6cae40d1e8c02e40321d49874bc42
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-24 23:40:22 +0600

    some ammendments are added

--- rest_countries.js
+++ rest_countries.js
@@ -14,16 +14,20 @@ const receivedRestCountries = (countries) => {
 
     const countriesContainer = document.getElementById("rest-countries-container");
     countries.forEach((country) => {
-        console.log(country.capital ? country.capital[0] : "not avilable");
+        // console.log(country);
+        const borders = country.borders;
+        console.log(borders);
         const div = document.createElement("div");
         div.classList.add("each-card");
-        div.innerHTML = `
+        div.innerHTML = ` 
         <div class="">
           <img class="card-image" src="${country.flags.png}" />
         <div class="">
           <h2 class="country-name">${country.name.common}</h2>
           <p>Capital : ${country.capital ? country.capital[0] : "capital is none"}</p>
+          <p>borders : ${borders ? borders.map((border) => border) : "border is none"}</p>
           <p>Continents : ${country.continents[0]}</p>
+          <p>Poputations : ${country.population} Area : ${country.area}</p>
 
           <div class="">
             <button class="">Buy Now</button>

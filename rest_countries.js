2b151f1b410b34382ba398aff937dd273267beda
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-25 12:49:50 +0600

    modal is installilng

--- rest_countries.js
+++ rest_countries.js
@@ -15,7 +15,8 @@ const receivedRestCountries = (countries) => {
   const countriesContainer = document.getElementById("rest-countries-container");
   countries.forEach((country) => {
     console.log(country);
-    // const borders = country.borders;
+    const languages = country.languages;
+    // console.log(languages);
     const div = document.createElement("div");
     div.classList.add("each-card");
     div.innerHTML = ` 
@@ -31,7 +32,7 @@ const receivedRestCountries = (countries) => {
               <p>Poputations : ${country.population}</p>
             </div>
             <div class="card-button-div">
-              <button class="card-button">Details</button>
+              <button class="card-button" onclick="loadCountriesDetails('${country.cca2}')">Details</button>
             </div>
           </div>
         </div>
@@ -41,6 +42,24 @@ const receivedRestCountries = (countries) => {
   })
 }
 
-// {/* <p>borders : ${borders ? borders.map((border) => border) : "border is none"}</p> */}
+const loadCountriesDetails = async (code) => {
+  const url = `https://restcountries.com/v3.1/alpha/${code}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCountriesDetails(data);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
 
+const displayCountriesDetails = (data) => {
+
+  console.log(data);
+}
+
+
+
+// {/* <p>borders : ${borders ? borders.map((border) => border) : "border is none"}</p> */}
 loadRestCountries();

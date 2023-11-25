b2440e55e3e251d6b659c58fcbe2210b4f5a3bde
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-25 10:34:06 +0600

    the button and card alignment are added

--- rest_countries.js
+++ rest_countries.js
@@ -1,42 +1,46 @@
 const loadRestCountries = async () => {
-    const url = `https://restcountries.com/v3.1/all`;
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        receivedRestCountries(data);
-    }
-    catch (error) {
-        console.log(error);
-    }
+  const url = `https://restcountries.com/v3.1/all`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    receivedRestCountries(data);
+  }
+  catch (error) {
+    console.log(error);
+  }
 }
 
 const receivedRestCountries = (countries) => {
 
-    const countriesContainer = document.getElementById("rest-countries-container");
-    countries.forEach((country) => {
-        // console.log(country);
-        const borders = country.borders;
-        console.log(borders);
-        const div = document.createElement("div");
-        div.classList.add("each-card");
-        div.innerHTML = ` 
-        <div class="">
+  const countriesContainer = document.getElementById("rest-countries-container");
+  countries.forEach((country) => {
+    console.log(country);
+    // const borders = country.borders;
+    const div = document.createElement("div");
+    div.classList.add("each-card");
+    div.innerHTML = ` 
+        <div class="card-whole">
           <img class="card-image" src="${country.flags.png}" />
         <div class="">
           <h2 class="country-name">${country.name.common}</h2>
-          <p>Capital : ${country.capital ? country.capital[0] : "capital is none"}</p>
-          <p>borders : ${borders ? borders.map((border) => border) : "border is none"}</p>
-          <p>Continents : ${country.continents[0]}</p>
-          <p>Poputations : ${country.population} Area : ${country.area}</p>
-
-          <div class="">
-            <button class="">Buy Now</button>
+          <div class="card-text">
+            <div class="card-information">
+              <p>Capital : ${country.capital ? country.capital[0] : "capital is none"}</p> 
+              <p>Continents : ${country.continents[0]}</p>
+              <p> Area : ${country.area}</p>
+              <p>Poputations : ${country.population}</p>
+            </div>
+            <div class="card-button-div">
+              <button class="card-button">Details</button>
+            </div>
           </div>
         </div>
       </div>
         `;
-        countriesContainer.appendChild(div);
-    })
+    countriesContainer.appendChild(div);
+  })
 }
+
+// {/* <p>borders : ${borders ? borders.map((border) => border) : "border is none"}</p> */}
 
 loadRestCountries();
